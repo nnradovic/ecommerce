62118a9926413413c3cce0de92d7d27b3c52edb3
Nikola Radovic <nikola.radovic@nultien.rs>
2020-05-06 21:30:36 +0200

    route-basic

---
+++ client/src/user/Signup.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../core/Layout'
+
+const Signup = () => {
+    return (
+        <Layout title="Signup" description=" Signup to Node React E-commerce App">
+            ...
+    </Layout>
+
+    )
+}
+
+export default Signup
+
