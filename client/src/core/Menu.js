5bab20ce7cd6126aa3fe67472f62e8b4091e5868
Nikola Radovic <nikola.radovic@nultien.rs>
2020-05-10 10:48:05 +0200

    admin-user-login

--- client/src/core/Menu.js
+++ client/src/core/Menu.js
@@ -18,6 +18,18 @@ const Menu = ({ history }) => {
                     <Link className="nav-link" to="/" style={isActive(history, '/')}
                     >Home</Link>
                 </li>
+                {isAuthenticated() && isAuthenticated().user.role === 0 && (
+                    <li className="nav-item" >
+                        <Link className="nav-link" to="/user/dashboard" style={isActive(history, '/user/dashboard')}
+                        > Dashboard</Link>
+                    </li>
+                )}
+                {isAuthenticated() && isAuthenticated().user.role === 1 && (
+                    <li className="nav-item" >
+                        <Link className="nav-link" to="/admin/dashboard" style={isActive(history, '/admin/dashboard')}
+                        >  Dashboard</Link>
+                    </li>
+                )}
                 {!isAuthenticated() && (
                     <Fragment>
                         <li className="nav-item">
--- client/src/user/Signin.js
+++ client/src/user/Signin.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Redirect } from 'react-router-dom'
 import Layout from '../core/Layout'
-import { signin, authenticate } from '../auth'
+import { signin, authenticate, isAuthenticated } from '../auth'
 
 const Signin = () => {
 
@@ -15,6 +15,7 @@ const Signin = () => {
     })
 
     const { email, password, loading, error, redirecToRefeerer } = values
+    const { user } = isAuthenticated()
     const handleChange = name => event => {
         setValues({ ...values, error: false, [name]: event.target.value })
     }
@@ -83,6 +84,14 @@ const Signin = () => {
 
     const redirectUser = () => {
         if (redirecToRefeerer) {
+            if (user && user.role === 1) {
+                return <Redirect to="/admin/dashboard" />
+            } else {
+                return <Redirect to="/user/dashboard" />
+            }
+        }
+
+        if (isAuthenticated()) {
             return <Redirect to="/" />
         }
     }
